749cf25f91af40540cf923cfe161c81a0bea09f2
vitordbzz <vitor_hugobatista@hotmail.com>
2017-06-08 12:45:40 -0300

    Foundation com a tabela treinamento

---
+++ P2YESOD/Foundation.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes,
+             TemplateHaskell, GADTs, FlexibleContexts,
+             MultiParamTypeClasses, DeriveDataTypeable,
+             GeneralizedNewtypeDeriving, ViewPatterns #-}
+module Foundation where
+import Yesod
+import Yesod.Static
+import Data.Text
+import Data.Time.Calendar
+import Database.Persist.Postgresql
+    ( ConnectionPool, SqlBackend, runSqlPool, runMigration )
+
+data Sitio = Sitio {getStatic :: Static, connPool :: ConnectionPool }
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Departamento
+   nome Text
+   sigla Text sqltype=varchar(3)
+   deriving Show
+
+Treinamento
+   nome Text
+   responsavel Text
+   sala Text
+   sigla Text sqltype=varchar(10)
+   qtdPessoas Int
+   dataVal Day
+   dataAplic Day
+   deriving Show
