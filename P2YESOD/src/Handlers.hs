7fbf4647935f722ac5921c74e43d8b1718260390
costalauro <costalauro@hotmail.com>
2017-06-08 14:03:42 -0300

    Handlers Form Funcionario

---
+++ P2YESOD/src/Handlers.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes,
+             TemplateHaskell #-}
+ 
+module Handlers where
+import Yesod
+import Foundation
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Applicative
+import Data.Text
+import Data.Time.Calendar
+
+import Database.Persist.Postgresql
+
+formFuncionario :: Form Funcionario
+formFuncionario = renderDivs $ Funcionario <$>
+             areq textField "Nome :" Nothing <*>
+             areq intField "Idade :" Nothing <*>
+             areq doubleField "Salário :" Nothing <*>
+             areq dayField "Data Nascimento :" Nothing <*>
+             areq (selectField dptos) "Departamentos :" Nothing <*>
+             areq (selectField profs) "Profissões :" Nothing
+
+dptos = do
+       entidades <- runDB $ selectList [] [Asc DepartamentoNome] 
+       optionsPairs $ fmap (\ent -> (departamentoSigla $ entityVal ent, entityKey ent)) entidades
+
+profs = do
+       entidades <- runDB $ selectList [] [Asc ProfissaoNome] 
+       optionsPairs $ fmap (\ent -> (profissaoSigla $ entityVal ent, entityKey ent)) entidades
+
+             
